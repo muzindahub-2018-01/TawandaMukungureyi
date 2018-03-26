700f0840f0553aa00906c4af246c2edef222319e
Tawanda Mukungureyi <37211610+Tawandah@users.noreply.github.com>
2018-03-26 15:51:40 +0200

    Project

---
+++ app/src/main/java/com/teamtreehouse/muzindaproject/MainActivity.java
@@ -0,0 +1,31 @@
+package com.teamtreehouse.muzindaproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnContinue);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            launchPersonalInfoActivity();
+            }
+        });
+    }
+
+    public void launchPersonalInfoActivity() {
+        Intent intent = new Intent(this, PersonalInfoActivity.class);
+        startActivity(intent);
+    }
+}
